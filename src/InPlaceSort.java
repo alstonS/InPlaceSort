be46cec953e1f3c84627d52467de5500923b0546
AlstonS <Ashi3454@bths.edu>
2018-11-19 01:19:44 -0500

    Done

--- src/InPlaceSort.java
+++ src/InPlaceSort.java
@@ -1,43 +1,98 @@
 public class InPlaceSort {
-
-    public static String[] stringArrayList(int length, int letters)
-    {
-
-
-
+    /**
+     *
+     * @param arr is the array
+     * @param pos1 the first number checked in array
+     * @param pos2  the number following the first number in array
+     */
+    public static void intSwap(int[] arr, int pos1, int pos2){
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
     }
-    public static double[] doubleArrayList(int length)
-    {
-
-
-
+    /**
+     *
+     * @param arr is the array
+     * @param pos1 the first number checked in array
+     * @param pos2  the number following the first number in array
+     */
+    public static void doubleSwap(double[] arr, int pos1, int pos2){
+        double temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
     }
-    public static int[] intArrayList(int length)
-    {
 
 
-
-    }
-
+    /**
+     *
+     * @param arr is generated in the runner.
+     */
     public static void bubbleSort(String[] arr)
     {
+        boolean checkStorted = false;
+        int a = arr.length-1;
+        while(!checkStorted)
+        {
+            checkStorted = true;
+            for(int i=0; i<a; i++)
+            {
+                if(arr[i].compareTo(arr[i+1]) > 0 )
+                {
+                    String Temp = arr[i+1];
+                    arr[i+1] = arr[i];
+                    arr[i] = Temp;
+                    checkStorted = false;
+                }
 
 
+            }
+            a--;
+        }
 
 
     }
+
+    /**
+     *
+     * @param arr created in the runner.
+     */
     public static void insertionSort(int[] arr)
     {
-
-
-
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                for(int j=i ; j>0 ; j--)
+                {
+                    if(arr[i]<arr[j])
+                    {
+                        intSwap(arr,j,i);
+                    }
+                    else { break; }
+                }
+            }
+        }
     }
+
+    /**
+     *
+     * @param arr created in the runner.
+     */
     public static void selectionSort(double[] arr)
     {
+        int temp;
+        for(int i=0;i<arr.length;i++){
+            temp = i;
+            for(int j=i+1;j<arr.length;j++) {
+                if (arr[temp] > arr[j]) {
+                    temp = j;
+                }
+            }
+            doubleSwap(arr, temp, i);
+        }
 
 
 
-        
     }
 
 
--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,93 @@
 public class Runner {
 
-    public static void main(String[] args)
-    {
-        double[] arrayDouble = InPlaceSort.doubleArrayList(1000);
-        String[] arrayString = InPlaceSort.stringArrayList(1000,4);
-        int[] arrayIntger = InPlaceSort.intArrayList(1000);
+    public static void main(String[] args) {
+        double[] arrayDouble = doubleArrayList(10000);
+        String[] arrayString = stringArrayList(10000);
+        int[] arrayInteger = intArrayList(10000);
+        long time;
 
+
+        //Selection
+        time = System.currentTimeMillis();
+        InPlaceSort.selectionSort(arrayDouble);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Selection Sort: "+ time*.001 + " Seconds");
+
+        //Insertion
+        time = System.currentTimeMillis();
+        InPlaceSort.insertionSort(arrayInteger);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Insertion Sort: "+ time*.001 + " Seconds" );
+
+        //Bubble
+        time = System.currentTimeMillis();
+        InPlaceSort.bubbleSort(arrayString);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Bubble Sort: "+ time*.001 + " Seconds");
+
+
+    }
+
+
+    /**
+     *
+     * @param length is given by user to determine length of list.
+     * @return the generated array with given length size
+     */
+    public static double[] doubleArrayList(int length)
+        {
+            double[] array = new double[length];
+            for(int i=0; i<array.length; i++)
+            {
+
+                array[i] = (Math.random()*1000);
+            }
+            return array;
+        }
+
+    /**
+     *
+     * @param length is given by user to determine length of list.
+     * @return the generated array with given length size
+     */
+        public static int[] intArrayList(int length)
+        {
+            int[] array = new int[length];
+            for( int i=0; i< array.length; i++)
+            {
+                array[i] = (int) (Math.random()*1000);
+
+            }
+            return array;
+        }
+
+
+    /**
+     *
+     * @param length is given by user to determine length of list.
+     * @return the generated array with given length size
+     */
+        public static String[] stringArrayList(int length) {
+
+            String[] array = new String[length];
+            for (int i = 0; i < array.length; i++) {
+                int randWordL = (int) (Math.random() * 10);
+                String word = "";
+                for (int j = 0; j < randWordL; j++) {
+                    int rando = (int) ((Math.random() * 10) + 1);
+                    char asd;
+                    if (rando > 5) {
+                        asd = (char) ((int) ((Math.random() * 26) + 65));
+                    } else {
+                        asd = (char) ((int) ((Math.random() * 26) + 97));
+                    }
+                    word += asd;
+
+                }
+                array[i] = word;
+            }
+            return array;
+        }
 
 
 
@@ -22,4 +104,6 @@ public class Runner {
 
 
 
-}
+
+
+
